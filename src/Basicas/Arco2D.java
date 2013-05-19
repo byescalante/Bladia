d2215937425987b875cfc78048dfa8a09f011125
angel <angelcrjv@gmail.com>
2013-05-19 04:35:02 -0500

    creando la clase arc2D

---
+++ src/Basicas/Arco2D.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basicas;
+
+/**
+ *
+ * @author Angel
+ */
+public class Arco2D {
+    
+    
+    
+    
+    
+}
