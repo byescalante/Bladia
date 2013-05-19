827db8605e5dfee89f15ba9b4be1badeb4e01c47
byescalante <byescalante@bydebian>
2013-05-19 06:19:24 -0500

    Terminando La clase Ellipse2D y Modificando CanvasJonathan ok

--- src/Test/CanvasJonathan.java
+++ src/Test/CanvasJonathan.java
@@ -5,102 +5,168 @@
 package Test;
 
 
+import Abstract.Strocks;
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
+import static Abstract.VarCustomCanvas.*;
+import Basicas.Square;
+import java.awt.AlphaComposite;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
 
 /**
  *
  * @author Amairani
  */
 public class CanvasJonathan extends Canvas {
-    private int centerX,centerY,left, right,top,bottom,xmiddle,ymiddle;
-    private float pixelSize,rHeight,rWidth;
-
-  
-    
-    public CanvasJonathan() {
-     
-        this.rHeight = 10.10F;
-        this.rWidth = 10.10F;
+    //====== Pinta el borde del poligono si la variable es verdadera(true).
+    private boolean draw = true;
+    //====== pinta el relleno del poligono si la variable es verdadera(true). 
+    private boolean fill = false;
+    //====== pinta el pligono con un color si la variable es verdadera(true).
+    private boolean isColor = true;
+     //====== pinta el poligono con un gradiente si la variable es verdadera(true).
+    private boolean isGradient = false;
+     //====== determina el poligono que se desea dibujar.
+    private int polygonPaint = 0;
+    //====== define un stroke por default.
+    private Stroke st = Strocks.simple;
+    //====== indica el tipo de strok a pintar.
+    private int asigStrock = 0;
+    //====== crea un objeto de tipo grandient.
+    private GradientPaint gradient;
+    //====== define el primer color del gradient.
+    private Color one = Color.BLUE;
+    //====== define el segundo color del gradient.
+    private Color two = Color.GREEN;
+    //====== color por default.
+    private Color c = Color.BLACK;
+    //====== composit por defualt.
+    private AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0F);
+    //private Square cu = new Square(-2, 2, 4, 4);
+    private Elipse2D eli = new Elipse2D(-2, 2, 4, 4);
+    /**
+     * 
+     * @param rwidth
+     * @param rheight 
+     */
+    public CanvasJonathan(float rwidth, float rheight) {
+        Abstract.VarCustomCanvas.rwidth = rwidth;
+        Abstract.VarCustomCanvas.rheight = rheight;
         setBackground(Color.WHITE);
+        addKeyListener(new KeyAdapter() {
+            
+            /**
+             * Devuelve un vento del teclado.
+             */
+            @Override
+            public void keyPressed(KeyEvent e) { 
+                /**
+                 * Mover a la Izquierda
+                 */
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    eli.moveLeft();
+                    repaint();
+                /**
+                 * Mover hacia la Derecha
+                 */    
+                }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+                    eli.moveRight();
+                    repaint();
+                /**
+                 * Mover hacia Arriba
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_UP){
+                    eli.moveUp();
+                    repaint();
+                /**
+                 * Mover hacia Abajo
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+                    eli.moveDwon();
+                    repaint();
+                /**
+                 * Rotar a la Izquierda
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_1){
+                    eli.RatationLeft();
+                    repaint();
+                /**
+                 * Rotar a la Derecha
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_2){
+                    eli.RatationRight();
+                    repaint();
+                /**
+                 * Scalar en Incremento
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_3){
+                    eli.ScaleUp();
+                    repaint();
+                /**
+                 * Escalar en Decremento
+                 */
+                }else if(e.getKeyCode() == KeyEvent.VK_4){
+                    eli.ScaleDown();
+                    repaint();
+                }
+                
+            }
+            
+        });
     }
-    public CanvasJonathan(float rWidth, float rHeight) {
-        super();
-        this.rHeight = rHeight;
-        this.rWidth = rWidth;
-        setBackground(Color.WHITE);
-    }
-    public void iniciarval(){
-       Dimension d = getSize();
-       int maxX = d.width-1;
-       int maxY = d.height-1;
-       centerX = maxX/2;
-       centerY = maxY/2;
-       pixelSize = Math.max(rWidth/maxX,rHeight/maxY);
-    }
-
+    /**
+     * 
+     * @param g Grafics principal sobre el que se pinta.
+     */
     @Override
-    public void paint(Graphics g) {
-        iniciarval();
-        pintarCuadro(g);
+    public void paint(Graphics g){
+        iniciarVAr(this);
+        BufferedImage image = new BufferedImage(maxX,maxY, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = (Graphics2D) image.createGraphics();
+        paintCuadricula(g2);
+        asignarV(g2);
+        eli.paint(g2, draw, fill);
+        g.drawImage(image, 0, 0, this);
     }
-   
-    public void pintarCuadro(Graphics g){
-       left = toFisicoX(-rWidth/2);
-       right = toFisicoX(rWidth/2);
-       top = toFisicoY(rHeight/2);
-       bottom = toFisicoY(-rHeight/2);
-        System.out.println(rHeight);
-       xmiddle = toFisicoX(0);
-       ymiddle = toFisicoY(0);
-       
-       g.setColor(Color.LIGHT_GRAY);
-        for (float i =-rWidth/2; i < rWidth/2; i+=0.1F) {
-             g.drawLine(toFisicoX(i),top,toFisicoX(i),bottom);
-            
+    /**
+     * 
+     * @param g2  un graphics donde se va a pintar.
+     */
+    public void asignarV(Graphics2D g2){
+        if (isColor) {
+            g2.setColor(c);
+        }else if(isGradient){
+            gradient = new GradientPaint(left, top, one, right, botton, two);
+            g2.setPaint(gradient);
         }
-        g.setColor(Color.LIGHT_GRAY);
-        for (float i = -rHeight/2; i < rHeight/2; i+=0.1F) {
-            g.drawLine(left, toFisicoY(i), right, toFisicoY(i));
-            
+        switch (asigStrock) {
+            case 0:
+                g2.setStroke(Strocks.simple);
+                break;
+            case 1:
+                g2.setStroke(Strocks.medio);
+                break;
+            case 2:
+                g2.setStroke(Strocks.completo);
+                break;
         }
-       
-       
-        for (int i =  Math.round(-rWidth/2); i < Math.round(rWidth/2); i++) {
-            g.drawLine(toFisicoX(i),top,toFisicoX(i),bottom);
-            g.drawString(""+i, toFisicoX(i),ymiddle);
-            g.setColor(Color.DARK_GRAY);
-            
-        }
-        for (int i = Math.round(-rHeight/2); i < Math.round(rHeight/2); i++) {
-            g.drawLine( left,toFisicoY(i), right,toFisicoY(i));
-             g.drawString(""+i, xmiddle, toFisicoY(i));
-            g.setColor(Color.DARK_GRAY);
-           
-            
-        }
-      g.setColor(Color.BLACK);
-       g.drawLine(left, top, right,top);
-       g.drawLine(right,top,right, bottom);
-       g.drawLine(right,bottom,left, bottom);
-       g.drawLine(left, bottom,left, top);
-       
-       g.setColor(Color.BLACK);
-       g.drawLine( xmiddle,top,xmiddle,bottom);
-       g.drawLine(left,ymiddle,right,ymiddle);
+        g2.setComposite(composite);
+        
     }
-   private int toFisicoX(float x){
-       return Math.round(centerX+x/pixelSize);
-   } 
-   private int toFisicoY(float y){
-       return Math.round(centerY-y/pixelSize);
-   } 
-   private float realX(int x){
-       return (x-centerX)*pixelSize;
-   }
-   private  float realY (int y){
-       return (centerY-y)*pixelSize;
-   }
+
+    /**
+     * 
+     * @param g Graphics que actualiza el pintado del canvas.
+     */
+    @Override
+    public void update(Graphics g) {
+        paint(g);
+    }
 }
